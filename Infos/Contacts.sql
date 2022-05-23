dbfb8fa0b6869f22f7372ee217216ae39ef96ac1
egrignouxlevert <ewan.grignoux56@gmail.com>
2022-05-23 16:32:08 +0200

    Update Contact.sql and add default user picture

--- Infos/Contacts.sql
+++ Infos/Contacts.sql
@@ -1,13 +1,21 @@
 CREATE TABLE Contact (
-	NoContact INTEGER PRIMARY KEY AUTOINCREMENT, 
-	Surnom VARCHAR(40) NOT NULL, Nom VARCHAR(40) , 
-	Prenom VARCHAR(40),
-	Groupe VARCHAR(10) CHECK (Groupe in ('Famille','Amis','Travail','Autre')),
-	Photo_profil VARCHAR(40) DEFAULT 'pour l instant jsp';
+	id INTEGER PRIMARY KEY AUTOINCREMENT, 
+	surName VARCHAR(40) NOT NULL,
+	name VARCHAR(40) , 
+	firstName VARCHAR(40),
+	groups VARCHAR(10) CHECK (Groupe in ('Famille','Amis','Travail','Autre')),
+	profilPicture VARCHAR(40) DEFAULT '../Pictures/default_users');
 	
 CREATE TABLE Coordonnees (
-	NoCoordonnees INTEGER PRIMARY KEY AUTOINCREMENT,
-	NumTelephone VARCHAR(10) NOT NULL, 
-	NoContact INTEGER NOT NULL, 
-	Emplacement VARCHAR(10) CHECK (Emplacement in ('Principal','Travail','Domicile','Autre')), 
-	CONSTRAINT fk_NoContact FOREIGN KEY (NoContact) REFERENCES Contact (NoContact) ON DELETE CASCADE ON UPDATE CASCADE);
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	phoneNumber VARCHAR(10) NOT NULL,
+	email VARCHAR(40) CHECK (email IS LIKE '%@%'),
+	idContact INTEGER NOT NULL, 
+	groups VARCHAR(10) CHECK (Emplacement in ('Principal','Travail','Domicile','Autre')),
+	socialMedia VARCHAR(100) ,
+	CONSTRAINT fk_idContact FOREIGN KEY (idContact) REFERENCES Contact (idContact) ON DELETE CASCADE ON UPDATE CASCADE);
+	
+CREATE TABLE Users (
+	login PRIMARY KEY VARCHAR(20) NOT NULL,
+	passwd VARCHAR(20) NOT NULL ,
+	status VARCHAR(20) DEFAULT 'user' CHECK (status in ('user','admin')));
