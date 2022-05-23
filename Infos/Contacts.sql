bd2a1cbc29e3e2b5a893f06f00eabae2eeba930e
egrignouxlevert <ewan.grignoux56@gmail.com>
2022-05-23 16:48:59 +0200

    ADD database with an admin account

--- Infos/Contacts.sql
+++ Infos/Contacts.sql
@@ -3,19 +3,19 @@ CREATE TABLE Contact (
 	surName VARCHAR(40) NOT NULL,
 	name VARCHAR(40) , 
 	firstName VARCHAR(40),
-	groups VARCHAR(10) CHECK (Groupe in ('Famille','Amis','Travail','Autre')),
+	groups VARCHAR(10) CHECK (groups in ('Famille','Amis','Travail','Autre')),
 	profilPicture VARCHAR(40) DEFAULT '../Pictures/default_users');
 	
 CREATE TABLE Coordonnees (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	phoneNumber VARCHAR(10) NOT NULL,
-	email VARCHAR(40) CHECK (email IS LIKE '%@%'),
+	email VARCHAR(40) CHECK (email LIKE '%@%'),
 	idContact INTEGER NOT NULL, 
-	groups VARCHAR(10) CHECK (Emplacement in ('Principal','Travail','Domicile','Autre')),
+	groups VARCHAR(10) CHECK (groups in ('Principal','Travail','Domicile','Autre')),
 	socialMedia VARCHAR(100) ,
 	CONSTRAINT fk_idContact FOREIGN KEY (idContact) REFERENCES Contact (idContact) ON DELETE CASCADE ON UPDATE CASCADE);
 	
 CREATE TABLE Users (
-	login PRIMARY KEY VARCHAR(20) NOT NULL,
+	login VARCHAR(20) PRIMARY KEY NOT NULL,
 	passwd VARCHAR(20) NOT NULL ,
 	status VARCHAR(20) DEFAULT 'user' CHECK (status in ('user','admin')));
